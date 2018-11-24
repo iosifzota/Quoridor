b477336059b54232ab491fd01834fdfe251bea78
Pitis Teodor <teodor.pitis@gmail.com>
2018-11-24 13:48:43 +0200

    Created using matrix header

--- Quoridor/Trench.h
+++ Quoridor/Trench.h
@@ -1,18 +1,13 @@
 #pragma once
-#include <array>
+#include "using_matrix.h"
 #include <algorithm>
-using std::array;
-using std::size_t;
-
-template < int N, int M, typename T>
-using matrix = array<array<T, M>, N>;
 
 template <size_t N, size_t M>
 class Trench
 {
 public:
 	Trench() =default;
-	matrix<N, M, bool> m_grid;
+	matrix<bool, N, M> m_grid;
 	bool AtForward(size_t i, size_t j) const
 	{
 		// FIX ME
