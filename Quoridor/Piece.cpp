2723bfac66037f889c34207ede672438512b870b
iosifzota <iosifzota@protonmail.com>
2018-11-27 09:26:48 +0200

    Piece: Added ApplyDirectionMask() [helper].

--- Quoridor/Piece.cpp
+++ Quoridor/Piece.cpp
@@ -1,22 +1,46 @@
 #include "Piece.h"
 
 const Position Piece::INVALID_POSITION{ -1, -1 };
+array<Position, 5> Piece::directionMask{};
 
 Piece::Piece() :
         m_position{-1, -1}
 {
-        // empty
+	InitDirectionMask();
 }
 Piece::Piece(const Position& p) :
         m_position{p.first, p.second}
 {
-        // empty
+	InitDirectionMask();
 }
 
-void Piece::SetPosition(const Position& p) {
-        m_position = p;
-}
-
-const Position& Piece::GetPosition() const {
+Position& Piece::AccessPosition() {
         return m_position;
+}
+
+static void addPosition(Position& dest, const Position& other)
+{
+	auto&[x_dest, y_dest] = dest;
+	auto&[x_other, y_other] = other;
+	x_dest += x_other;
+	y_dest += y_other;
+}
+
+void Piece::ApplyDirectionMask(Position& pos, Piece::Direction direction)
+{
+	addPosition(pos, directionMask[(int)direction]);
+}
+
+// FIX ME
+void Piece::InitDirectionMask()
+{
+	static bool initalized = false;
+
+	if (initalized) return;
+
+	directionMask.at((int)Direction::North).first = -1;
+	directionMask.at((int)Direction::South).first = +1;
+	directionMask.at((int)Direction::East).second = -1;
+	directionMask.at((int)Direction::None).second = +1;
+	initalized = true;
 }
--- Quoridor/Piece.h
+++ Quoridor/Piece.h
@@ -1,27 +1,32 @@
 #pragma once
 
 #include <utility>
-
+#include <array>
+using std::array;
 using Position = std::pair<int, int>;
 
 class Piece {
 public:
-        // Used by Pawn and Wall
-        enum class Direction : char {
-                None,
-                North,
-                South,
-                East,
-                West
-        };
-		static const Position INVALID_POSITION;
+	// Used by Pawn and Wall
+	enum class Direction : char {
+		None,
+		North,
+		South,
+		East,
+		West
+	};
+	static const Position INVALID_POSITION;
 
-        Piece();
-        explicit Piece(const Position&);
+	Piece();
+	explicit Piece(const Position&);
 
-        void SetPosition(const Position&);
-        const Position& GetPosition() const;
+	Position& AccessPosition();
 
+	static void ApplyDirectionMask(Position&, Piece::Direction);
 protected:
-        Position m_position;
+	Position m_position;
+
+private:
+	static array<Position, 5> directionMask;
+	void InitDirectionMask(); // FIX ME
 };
