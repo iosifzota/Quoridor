fdc14652a68d82cd82511ba129f5f00a6bcd08f5
Pitis Teodor <teodor.pitis@gmail.com>
2018-11-30 13:42:07 +0200

    Class PlaceableWall fix GetPawn() and added setters and getters

--- Quoridor/PlaceableWall.cpp
+++ Quoridor/PlaceableWall.cpp
@@ -9,8 +9,35 @@ PlaceableWall::PlaceableWall(const Position& pos, Direction direction) :
 	/* empty */
 }
 
-PlaceableWall& PlaceableWall::GetInstance(Position pos, Direction direction)
+void PlaceableWall::SetDirection(Direction direction)
 {
+	if (!Piece::validDirection(direction))
+		throw "invalid direction";
+	m_direction = direction;
+}
+
+void PlaceableWall::SetPosition(const Position& position)
+{
+	if (!Piece::validPosition(position))
+		throw "invalid position";
+	m_position = position;
+}
+
+Piece::Direction PlaceableWall::GetDirection() const
+{
+	return m_direction;
+}
+
+const Position& PlaceableWall::GetPosition() const
+{
+	return m_position;
+}
+
+OptRef<PlaceableWall> PlaceableWall::GetInstance(const Position& pos, Direction direction)
+{
+	if (!Piece::validDirection(direction) || !Piece::validPosition(pos))
+		return {};
+
 	m_instance.m_direction = direction;
 	m_instance.m_position = pos;
 	return m_instance;
--- Quoridor/PlaceableWall.h
+++ Quoridor/PlaceableWall.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Piece.h"
+#include "util_decls.h"
 
 class PlaceableWall:
 	public Piece
@@ -9,10 +10,15 @@ private:
 	static PlaceableWall m_instance;
 	PlaceableWall(const Position&, Direction);
 
+	void SetDirection(Direction);
+	void SetPosition(const Position&);
+	Direction GetDirection() const;
+	const Position& GetPosition() const;
+
 private:
 	Direction m_direction;
 
 public:
-	static PlaceableWall& GetInstance(Position, Direction);
+	static OptRef<PlaceableWall> GetInstance(const Position&, Direction);
 };
 
