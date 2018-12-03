afabae984d595724e83b00756bceaa366ab12e3b
iosifzota <iosifzota@protonmail.com>
2018-12-03 23:51:29 +0200

    GameState.h: More utilities.

--- Quoridor/GameState.h
+++ Quoridor/GameState.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "req.hh"
+#include "util_templates.h"
 #include <string_view>
 using std::string_view;
 
@@ -10,7 +10,16 @@ enum class GameState :char {
 	Quiting
 };
 
-using ParseState = std::pair<GameState, char>;
+//using ParseState = std::pair<GameState, char>;
+
+template<GameState>
+struct ParsedValue
+{
+	ParsedValue(char character) : c{ character } {};
+	const char c;
+
+	operator char() { return c; }
+};
 
 namespace Parse {
 	constexpr static char moving[] = "wasdWASD";
@@ -34,13 +43,17 @@ namespace Parse {
 	}
 }
 
+// idea: and_if
+
 template <GameState gameState>
 class GameStateParser
 {
+	using LocalParsedValue = ParsedValue<gameState>;
+
 public:
 	constexpr GameStateParser() = default;
 
-	inline constexpr variant<ParseState, string_view> operator()(const string_view& line) const
+	inline constexpr variant<LocalParsedValue, string_view> operator()(const string_view& line) const
 	{
 		if (line.empty()) {
 			return line;
@@ -51,7 +64,7 @@ public:
 			return line;
 		}
 
-		return ParseState(gameState, line[optionIndex]);
+		return LocalParsedValue(line[optionIndex]);
 	}
 
 private:
@@ -63,4 +76,47 @@ namespace Parse {
 	constexpr static GameStateParser<GameState::Moving> Moving;
 	constexpr static GameStateParser<GameState::Placing> Placing;
 	constexpr static GameStateParser<GameState::Quiting> Quiting;
+}
+
+
+template<typename Result, GameState gameState>
+using Function = ignore_deduction_t<function<Result(ParsedValue<gameState>&, GameState)>>;
+
+struct Success {}; // tag
+
+template<GameState gameState>
+variant<Success, string_view> applyParser(
+	const string_view& line,
+	const GameStateParser<gameState>& parser)
+{
+	using LocalParsedValue = ParsedValue<gameState>;
+
+	// [Debug]
+	static_assert(sizeof(string_view) == sizeof(string_view&));
+
+	auto parseResult = parser(line); // <?, string_view>
+	if (std::holds_alternative<LocalParsedValue>(parseResult)) {
+		return Success{};
+
+	}
+	return std::get<string_view>(parseResult);
+}
+
+template<typename Result, GameState gameState>
+variant<Result, string_view> applyParser(
+	const string_view& line,
+	const GameStateParser<gameState>& parser,
+	Function<Result, gameState> action)
+{
+	using LocalParsedValue = ParsedValue<gameState>;
+
+	// [Debug]
+	static_assert(sizeof(string_view) == sizeof(string_view&));
+
+	auto parseResult = parser(line); // <?, string_view>
+	if (std::holds_alternative<LocalParsedValue>(parseResult)) {
+		return action(std::get<LocalParsedValue>(parseResult), gameState);
+
+	}
+	return std::get<string_view>(parseResult);
 }
